aca2400859ffc40b3417f6089ad0fdbd32797e75
Antoine Chevalier <53263615+SeyBoo@users.noreply.github.com>
2023-04-19 15:12:52 +0200

    add nav

---
+++ src/common/components/src/nav.tsx
@@ -0,0 +1,16 @@
+import LogoImage from "@/common/assets/logo.svg";
+import Image from "next/image";
+import { FunctionComponent } from "react";
+
+export const Nav: FunctionComponent = () => {
+  return (
+    <nav className="flex items-center h-24 -ml-4 lg:h-32 lg:-ml-8">
+      <div className="relative h-[150px] w-[150px] lg:h-[200px] lg:w-[200px]">
+        <Image src={LogoImage} alt="logo" fill />
+      </div>
+      <p className="text-[#DD9787] font-semibold text-xl -ml-16 -mt-2 lg:-ml-[85px] lg:-mt-4 lg:text-2xl">
+        Antoine Chevalier <strong className="text-[#FFCB6B80]">.</strong>
+      </p>
+    </nav>
+  );
+};
